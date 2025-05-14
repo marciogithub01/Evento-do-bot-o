86b2abe8f3f42fecc41d245f26c54a39ab3982de
marciogithub01 <marcio.rr@hotmail.com.br>
2025-05-14 10:55:20 -0300

    first commit

--- src/components/Condicional.jsx
+++ src/components/Condicional.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import App from "../App";
 
 export default function Condicional() {
     const [email, setEmail] = useState("");
@@ -8,19 +9,32 @@ export default function Condicional() {
         console.log(email);   
     }
 
+    function limparEmail() {
+        setEmail("");
+    }
+
     return (
         <div>
-            <h2>Cadastre seu email</h2>
+            <h2>Cadastre seu Email</h2>
             <form onSubmit={enviarEmail}>
                 <input
                     type="email"
-                    placeholder="Digite seu email"
+                    placeholder="Digite seu Email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)} 
                     required 
                 />
-                <button type="submit">Enviar</button>
+                <button type="submit">Enviar-Email</button>
+                      {email && (
+                <div>
+                    <p>O email {email} foi cadastrado com sucesso!</p>
+                    <button onClick={limparEmail}>Limpar</button>
+                </div>
+            )}
             </form>
+      
         </div>
     );
 }
+
+// import { useState } from "react";
